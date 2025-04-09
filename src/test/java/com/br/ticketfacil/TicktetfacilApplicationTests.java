0bec940c25642c0e087aed5cc44288d1fb5bbc39
devlucasalmd <lucas.amaral.almd@gmail.com>
2025-04-09 19:07:02 -0300

    first commit

---
+++ src/test/java/com/br/ticketfacil/TicktetfacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ticketfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicktetfacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
